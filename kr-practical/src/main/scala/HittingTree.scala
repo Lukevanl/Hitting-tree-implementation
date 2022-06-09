ece41de5b09d0779b95b047d17e8575b56e57102
lukev <lukevl2203@gmail.com>
2022-06-09 11:38:07 +0200

    Add comments, minor fixes and changes in variable names

--- kr-practical/src/main/scala/HittingTree.scala
+++ kr-practical/src/main/scala/HittingTree.scala
@@ -8,30 +8,29 @@ case class HittingTree[+T](value: List[FOLTerm], children: List[HittingTree[T]])
 }
 
 object HittingTree {
-  //Create tree where you keep visited components in left of tuple, children on the right
-  def makeHittingTree(all_hs : List[List[FOLTerm]], visited_comp: List[FOLTerm]): HittingTree[Any] = all_hs match {
+  //Create tree from list of conflict sets (all_cs) where you keep visited components in left of tuple and children on the right
+  def makeHittingTree(all_cs : List[List[FOLTerm]], visited_comp: List[FOLTerm]): HittingTree[Any] = all_cs match {
     case List() => HittingTree(visited_comp, List())
     case List(List()) => HittingTree(visited_comp, List())
     //case List(List(), _) => HittingTree(visited_comp, List())
-    case all_hs => HittingTree(visited_comp, (for (hs_component <- all_hs.head) yield (if (visited_comp.contains(hs_component)) HittingTree(visited_comp, List()) else makeHittingTree(all_hs.tail, visited_comp ++ List(hs_component)))))
+    case all_cs => HittingTree(visited_comp, (for (cs_component <- all_cs.head) yield (if (visited_comp.contains(cs_component)) HittingTree(visited_comp, List()) else makeHittingTree(all_cs.tail, visited_comp ++ List(cs_component)))))
   }
-
+  //Read off hitting sets from hitting tree generated in makeHittingTree() 
   def gatherHittingSets(hitting_tree: HittingTree[Any]): List[List[FOLTerm]] = hitting_tree match {
       case HittingTree(comp, List()) => List(comp)
       //case HittingTree(comp, List(HittingTree(comp_found, children))) => (for (child <- children) yield gatherHittingSets(child)).concatenate
       case HittingTree(comp, trees) => ThreeDimConcat((for (tree <- trees) yield gatherHittingSets(tree))).filter(_ != List())
   }
-
+  //Helper function to concat 3 dimensional list into 2 dimensional
   def ThreeDimConcat(list: List[List[List[FOLTerm]]]): List[List[FOLTerm]] = list match {
       case List() => List(List())
       case List(List()) => List(List())
       case xs::xss => xs ++ ThreeDimConcat(xss)
-      //case _ => List(List())
   }
-
+  //Remove supersets from list of hitting sets
   def getDiagnosis(HS: List[List[FOLTerm]]): List[List[FOLTerm]] = HS match {
       case List(List(x)) => List(List(x))
-      case HS => for (set1 <- HS; if !(HS.exists(x => {x.forall(set1.contains(_)) && (x.length < set1.length)}))) yield set1 //TODO: fix this line
+      case HS => for (set1 <- HS; if !(HS.exists(x => {x.forall(set1.contains(_)) && (x.length < set1.length)}))) yield set1
       case _ => List(List())
   }
 
--- kr-practical/src/main/scala/Main.scala
+++ kr-practical/src/main/scala/Main.scala
@@ -13,19 +13,21 @@ object Main extends App {
   val hs = List()
   val Some(result) = tpf(problem2)
   println(result)
-  val HS_problem3 = List(List(fot"a1", fot"a2") , List(fot"a1", fot"o1"))
-  // for (hs_component <- HS_problem3.head) yield println(hs_component)
-  // println(HS_problem3.tail)
-  //val HS_problem3 = List(List(), List())
-  //println(HS_problem3)
-  val Htree = makeHittingTree(HS_problem3, List())
+  //Conflict sets of the problem
+  val CS_problem3 = List(List(fot"a1", fot"a2") , List(fot"a1", fot"o1"))
+  //Generate hitting tree from these conflict sets
+  val Htree = makeHittingTree(CS_problem3, List())
   println("Hitting tree:")
   println(Htree)
+  //Gather hitting sets from the hitting tree by reading of the leaf values
   val HS = gatherHittingSets(Htree)
   println("Hitting sets read off the tree:")
   println(HS)
   println("Removed supersets:")
-  println(getDiagnosis(HS))
+  val diagnosis = getDiagnosis(HS)
+  println(diagnosis)
+
+  
   //println("Running diagnostics with the previous result assumed to be a broken component..")
   //val Some(result2) = tpf(problem2, List(fot"a1"))
   //println(result2)
